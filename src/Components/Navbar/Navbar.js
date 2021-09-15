b2813f886b30741ea82965aae974a11886575891
jiahern <jiahern@student.unimelb.edu.au>
2021-09-15 19:30:23 +0800

    Navbar Partiallty Done

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -10,14 +10,14 @@ function Navbar() {
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
   return (
-    <>
+    <IconContext.Provider value={{ color: "#fff" }}>
       <div className="navbar">
         <Link to="#" className="menu-bars">
           <FaIcons.FaBars onClick={showSidebar} />
         </Link>
       </div>
       <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-        <ul className="nav-menu-items">
+        <ul className="nav-menu-items" onClick={showSidebar}>
           <li className="navbar-toggle">
             <Link to="#" className="menu-bars">
               <AiIcons.AiOutlineClose />
@@ -35,7 +35,7 @@ function Navbar() {
           })}
         </ul>
       </nav>
-    </>
+    </IconContext.Provider>
   );
 }
 
