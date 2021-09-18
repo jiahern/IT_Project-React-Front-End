779a291d39f3001dc9272eb11dc83f36e011fc95
jiahern <jiahern@student.unimelb.edu.au>
2021-09-18 14:41:26 +0800

    navbar Done

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      HomePage
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
+    </div>
+  );
 }
 
 export default HomePage;
