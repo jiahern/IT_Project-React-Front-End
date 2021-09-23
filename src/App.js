a9672275c91d10c15a81227cae96947ac13179f3
jiahern <jiahern@student.unimelb.edu.au>
2021-09-23 17:10:03 +0800

    merge

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {
 import React, { useEffect, useState } from "react";
 import LoginForm, { Logout } from "./Components/Login/login";
 import HomePage from "./Pages/HomePage";
-import Union from "./Pages/Union";
+import Union from "./Components/Union/Union";
 import Linkage from "./Pages/Linkage";
 import Task from "./Pages/Task";
 import Calendar from "./Pages/Calendar";
