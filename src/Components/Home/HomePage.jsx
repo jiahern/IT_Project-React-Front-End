df11b10fc863256536989e0baa8d5e98cbb252c8
allanz0303 <77386438+allanz0303@users.noreply.github.com>
2021-09-28 15:58:57 +1000

    Reading data from database

--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -20,6 +20,46 @@ const HomePage = () => {
         
         </div>
 
+        {/* {foods.map((item, index) => {
+          return (
+            <section>
+              <div className="Linkage w-full h-full mr-4 px-20 py-6 flex flex-col bg-blue-100 grid grid-cols-5 gap-x-4 gap-y-4">
+                <img className="w-20 h-20" src={linkage} />
+                <span className="py-6">
+                  {item.firstName + " " + item.middleName + " " + item.lastName}
+                </span>
+
+                <div className="FrindSince h-5 ml-2 py-6 px-6">
+                  {item.linkedSince}
+                </div>
+                <div className="LastInTouch h-5 ml-4 py-6 px-6">
+                  {item.lastConnection}
+                </div>
+                
+              </div>
+            </section>
+          );
+        })} */}
+
+        {/* {unionContents.map((item, index) => {
+          return (
+            <div
+              key={index}
+              class=" PendingTasks w-full h-full mr-4 px-20 py-6 flex flex-col bg-blue-100 grid grid-cols-4 gap-x-4 gap-y-4"
+            >
+              <img
+                class="w-20 h-20"
+                src={BASE_URL + item.profilePic}
+                alt="Union Profile Pic"
+              />
+              <span class="py-6">{item.name}</span>
+              <div class="Category h-5 ml-4 py-6 px-6">
+                {item.linkages.length}
+              </div>
+            </div>
+          );
+        })} */}
+
         <div className="Number w-full h-16 mr-4 px-20 py-6 flex flex-col bg-blue-100 grid grid-cols-3 grid-rows-1 gap-x-24">
 
             <div className="LinkageCount font-bold">8</div>
