76e82f833c9f7497c4ca457f440170469f51a7f1
chaisengl <chaisengl@student.unimelb.edu.au>
2021-10-06 18:53:39 +1100

    groupped task and events in Calander/Schedular, apply them with different color

--- src/api.js
+++ src/api.js
@@ -496,13 +496,17 @@ function userCalendar() {
     responseTask.forEach(element => {
       element.type = "Task";
       element.Subject = element.name;
+      element.ResourceID = 1;
       element.IsAllDay = false;
+      // element.IsReadonly = true;
     });
     responseEvent.forEach(element => {
       element.type = "Event";
       element.Subject = element.name;
+      element.ResourceID = 2;
       // element.StartTime = new Date(element.dateTime);
       element.IsAllDay = false;
+      // element.IsReadonly = true;
     });
     const response_merge = responseEvent.concat(responseTask);
     const sortedResponse = response_merge.sort((a, b) => new Date(a.StartTime) - new Date(b.StartTime))
