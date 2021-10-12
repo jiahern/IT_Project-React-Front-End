45b7feb42f04c87f073fb9f9d6bb25ee66747a13
allanz0303 <77386438+allanz0303@users.noreply.github.com>
2021-10-12 18:21:27 +1100

    Update Profile.jsx

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -6,11 +6,12 @@ const Profile = () => {
     
     return (
         <React.Fragment children>
+            <div className="font-bold text-4xl  ml-20">Account Profile</div>
             {profile.map((item, index) => {
                 return (
                 <div key={index}>
                     
-                    <div class="py-6">Password: {item.password}</div>
+                    {/*<div class="py-6">Password: {item.password}</div>*/}
                     <div class="py-6">Email: {item.email}</div>
                     <div class="py-6">FirstName: {item.firstName}</div>
                     <div class="py-6">LastName: {item.lastName}</div>
@@ -21,7 +22,21 @@ const Profile = () => {
                 );
             })}
 
+            <button
+            
+            className="newUnion border-x border-black font-bold rounded mr-10"
+            id="createTask"
+            >
+            Edit Profile
+            </button>
 
+            <button
+            
+            className="newUnion border-x border-black font-bold rounded mr-10"
+            id="createTask"
+            >
+            Change Password
+            </button>
 
 
         </React.Fragment>
